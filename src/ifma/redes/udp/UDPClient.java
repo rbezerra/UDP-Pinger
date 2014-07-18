d49a4d7ac72b68fbcd32b9da46e58a5103f31e74
Ramon Bezerra <ramonbezerra90@gmail.com>
2014-07-18 15:35:36 -0300

    inicio da classe UDPClient

--- src/ifma/redes/udp/UDPClient.java
+++ src/ifma/redes/udp/UDPClient.java
@@ -3,13 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ifma.redes.udp;
+
+import java.net.DatagramSocket;
+import java.util.Scanner;
 
 /**
  *
  * @author rbezerra
  */
 public class UDPClient {
+
+    private final static int PACKETSIZE = 100;
+
+    public static void main(String[] args) {
+        DatagramSocket socket = null;
+
+        try {
+            Scanner in = new Scanner(System.in);
+            String cap = in.nextLine();
+            if (ipvalido(cap)) {
+                String[] address = cap.split(":");
+                String part_host = address[0];
+                int part_port = Integer.parseInt(address[1]);
+
+                System.out.println("host: " + part_host + "\nport: " + part_port);
+            }else{
+                System.out.println("IP INVALIDO");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
     
+    static boolean ipvalido(String ip){
+        return ip.matches("(\\d+\\.\\d+\\.\\d+\\.\\d+):(\\d+)");
+    }
 }
