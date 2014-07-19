80586a855cde29610ef4583d8e47f5665a514eb7
psinalberth <inalberth07@gmail.com>
2014-07-19 05:21:28 -0300

    Classe UDPServer adicionada.

--- src/ifma/redes/udp/UDPClient.java
+++ src/ifma/redes/udp/UDPClient.java
@@ -5,7 +5,9 @@
  */
 package ifma.redes.udp;
 
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.Scanner;
 
 /**
@@ -26,14 +28,38 @@ public class UDPClient {
                 String[] address = cap.split(":");
                 String part_host = address[0];
                 int part_port = Integer.parseInt(address[1]);
-
+                
                 System.out.println("host: " + part_host + "\nport: " + part_port);
+                
+                InetAddress host = InetAddress.getByName(part_host);
+                
+                socket = new DatagramSocket();
+                
+                byte [] data = "Hello server".getBytes();
+                
+                DatagramPacket packet = new DatagramPacket(data, data.length, host, part_port);
+                
+                socket.send(packet);
+                socket.setSoTimeout(3000);
+                
+                packet.setData(new byte[PACKETSIZE]);
+                socket.receive(packet);
+                
+                System.out.println(new String(packet.getData()));
+                
             }else{
                 System.out.println("IP INVALIDO");
             }
 
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
+            
+        } finally {
+            
+            if (socket != null) {
+                
+                socket.close();
+            }
         }
     }
     
