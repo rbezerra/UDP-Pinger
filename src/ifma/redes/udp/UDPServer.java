6623cca0ffbdca4bb99db636b8d92e1f596ab98e
psinalberth <inalberth07@gmail.com>
2014-07-19 04:34:08 -0300

    Classe UDPServer adicionada.

--- src/ifma/redes/udp/UDPServer.java
+++ src/ifma/redes/udp/UDPServer.java
@@ -6,10 +6,41 @@
 
 package ifma.redes.udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 /**
  *
  * @author rbezerra
  */
 public class UDPServer {
     
+    private static final int PORT = 9192;
+    private static final int SIZE = 100;
+
+    public static void main(String [] args) {
+
+        try {
+
+            DatagramSocket socket = new DatagramSocket(PORT);
+
+            System.out.println("Server Up!");
+
+            for (; ;) {
+
+                DatagramPacket packet = new DatagramPacket(new byte[SIZE], SIZE);
+
+                socket.receive(packet);
+
+                System.out.println(packet.getAddress() + " " + packet.getPort() + ": " + new String(packet.getData()));
+
+                socket.send(packet);
+            }
+            
+        } catch (IOException e) {
+
+            System.out.println(e.getMessage());
+        }
+    }
 }
